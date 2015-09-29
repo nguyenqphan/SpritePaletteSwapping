57988534d47932e3ea30eb19132fa5e530353d7d
Nguyen Phan <nqphan212@gmail.com>
2015-09-29 15:26:56 -0400

    Build custom menus

---
+++ Assets/Scripts/ColorPalette.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using UnityEditor;
+
+[Serializable]
+public class ColorPalette : ScriptableObject {
+
+	[MenuItem("Assets/Create/Color Palette")]
+	public static void CreateColorPalette(){
+
+		if (Selection.activeObject is Texture2D) {
+			Debug.Log ("Creating a Palette");
+		} else {
+			Debug.Log("Cant create a Palette");
+		}
+	}
+}
