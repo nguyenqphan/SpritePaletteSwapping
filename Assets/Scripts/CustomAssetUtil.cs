185df84221ff1114c55762ca51f35d41d21b4bcf
Nguyen Phan <nqphan212@gmail.com>
2015-09-29 18:15:29 -0400

    Display custom color palette data

--- Assets/Scripts/CustomAssetUtil.cs
+++ Assets/Scripts/CustomAssetUtil.cs
@@ -2,22 +2,23 @@
 using System.Collections;
 using UnityEditor;
 
-public class CustomAssetUtil : MonoBehaviour {
-
+public class CustomAssetUtil {
+	
 	public static T CreateAsset<T>(string path) where T : ScriptableObject{
-
+		
 		T asset = null;
-
-		asset = ScriptableObject.CreateInstance<T>();
-
+		
+		asset = ScriptableObject.CreateInstance<T> ();
+		
 		var newPath = AssetDatabase.GenerateUniqueAssetPath (path);
-
+		
 		AssetDatabase.CreateAsset (asset, newPath);
-
+		
 		AssetDatabase.SaveAssets ();
-
+		
 		return asset;
-
+		
 	}
-
+	
+	
 }
