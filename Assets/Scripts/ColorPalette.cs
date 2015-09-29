fe53bc3aac24c064008a05b1ad3a3e690047ea89
Nguyen Phan <nqphan212@gmail.com>
2015-09-29 16:36:14 -0400

    Get paths to assets

--- Assets/Scripts/ColorPalette.cs
+++ Assets/Scripts/ColorPalette.cs
@@ -10,7 +10,13 @@ public class ColorPalette : ScriptableObject {
 	public static void CreateColorPalette(){
 
 		if (Selection.activeObject is Texture2D) {
-			Debug.Log ("Creating a Palette");
+
+			var selectedTexture = Selection.activeObject as Texture2D;
+
+			var selectionPath = AssetDatabase.GetAssetPath(selectedTexture);
+
+			selectionPath = selectionPath.Replace(".png", "-color-palette.asset");
+			Debug.Log ("Creating a Palette"+selectionPath);
 		} else {
 			Debug.Log("Cant create a Palette");
 		}
