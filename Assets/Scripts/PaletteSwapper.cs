4813b8a5a26c1627fce4f09773e3e8ce7364fabf
Nguyen Phan <nqphan212@gmail.com>
2015-09-29 21:07:35 -0400

    Clone a texture

---
+++ Assets/Scripts/PaletteSwapper.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaletteSwapper : MonoBehaviour {
+	
+	public SpriteRenderer spriteRenderer;
+	public ColorPalette[] palettes;
+	
+	private Texture2D texture;
+	private Texture2D cloneTexture;
+	
+	void Awake(){
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+		texture = spriteRenderer.sprite.texture;
+		
+		var w = texture.width;
+		var h = texture.height;
+		
+		cloneTexture = new Texture2D (w, h);
+		cloneTexture.wrapMode = TextureWrapMode.Clamp;
+		cloneTexture.filterMode = FilterMode.Point;
+		
+		Color[] pixels = texture.GetPixels ();
+		
+		cloneTexture.SetPixels (pixels);
+		cloneTexture.Apply ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
